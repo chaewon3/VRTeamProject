fe5cd496af4af5a9834b6e2fb44ea8bade44bad5
HyunggyuPyo <vygudrb@naver.com>
2024-07-09 21:25:49 +0900

    총 스크립트 추가

---
+++ Assets/Project/Pyo/ShootGun.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ShootGun : MonoBehaviour
+{
+    BulletPool pool;
+
+    //public ActionBasedController targetCont;
+    ActionBasedController targetCont;
+    private InputActionReference activateRef;
+
+    private void Awake()
+    {
+        pool = FindObjectOfType<BulletPool>();
+        //targetCont = FindObjectOfType<ActionBasedController>();
+        targetCont = GameObject.Find("Right Controller").GetComponent<ActionBasedController>();
+        activateRef = targetCont.activateAction.reference;
+    }
+
+    public void OnActivate()
+    {
+        pool.Shoot();
+    }
+
+    private void OnEnable()
+    {
+        activateRef.action.performed += OnActivateEventCall;
+    }
+    private void OnDisable()
+    {
+        activateRef.action.performed -= OnActivateEventCall;
+    }
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForEndOfFrame();
+        //activateRef.action.performed -= OnActivateEventCall;
+        //activateRef.action.performed += OnActivateEventCall;
+        ///*delegate (InputAction.CallbackContext context)
+        //{
+        //    TriggerActivate(context.performed);
+        //};*/
+
+    }
+
+    private void OnActivateEventCall(InputAction.CallbackContext cont)
+    {
+        TriggerActivate(cont.performed);
+    }
+
+
+    public void TriggerActivate(bool isPush)
+    {
+        if (isPush)
+        {
+            pool.Shoot();
+        }
+
+    }
+}
