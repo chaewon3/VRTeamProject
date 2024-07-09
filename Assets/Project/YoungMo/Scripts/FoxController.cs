9bbe3d30242b8f0bc6df4678826abcc885df76b4
SungYoungMo <dudah5193@naver.com>
2024-07-10 03:45:26 +0900

    숫자 뽑기 완성

--- Assets/Project/YoungMo/Scripts/FoxController.cs
+++ Assets/Project/YoungMo/Scripts/FoxController.cs
@@ -10,6 +10,7 @@ public class FoxController : MonoBehaviour
     Transform henTransform;
     public Transform foxSpawnPoint;
     int foxIndex;
+    public float timeToAttack = 3.0f; 
 
     // 이렇게 해놓고 오브젝트 풀링을 통해
     // 닭과 여우 스폰 위치를 저장하고 활성화 될때 부여하기
@@ -41,11 +42,8 @@ public class FoxController : MonoBehaviour
         {
             print("at null");
         }
-        //at.DisspearCoroutine();
         yield return new WaitForSeconds(at.duration);
 
-        AnimalPooling.instance.usingIndex.Add(foxIndex);
-        print($"foxindex : {foxIndex}");
         canMove = true;
 
     }
@@ -82,7 +80,8 @@ public class FoxController : MonoBehaviour
 
     IEnumerator FoxAttack()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(timeToAttack);
+        AnimalPooling.instance.foxIndexArr[foxIndex] = 0;
         gameObject.SetActive(false);
     }
 
